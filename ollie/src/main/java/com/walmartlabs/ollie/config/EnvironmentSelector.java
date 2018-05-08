f67d10a2c0f1c8df7ba48b853c50d6c451b5b3fc
Jason van Zyl <jason@vanzyl.ca>
2018-05-07 19:03:36 -0500

    Set to a development environment if the ~/.ollie directory is present

--- ollie/src/main/java/com/walmartlabs/ollie/config/EnvironmentSelector.java
+++ ollie/src/main/java/com/walmartlabs/ollie/config/EnvironmentSelector.java
@@ -1,11 +1,18 @@
 package com.walmartlabs.ollie.config;
 
+import java.io.File;
+
 public class EnvironmentSelector {
   
   public final static String ENVIRONMENT_KEY = "ollie.environment";
+  public final static File OLLIE_DIRECTORY = new File(System.getProperty("user.home"), ".ollie");
   
-  public Environment select() {
-    String environmentName = System.getProperty(ENVIRONMENT_KEY, Environment.DEVELOPMENT.id());
+  public Environment select() {    
+    String environmentName;
+    if(OLLIE_DIRECTORY.exists()) {
+      environmentName = Environment.DEVELOPMENT.id();
+    }
+    environmentName = System.getProperty(ENVIRONMENT_KEY, Environment.DEVELOPMENT.id());    
     return Environment.fromName(environmentName);
   }
 }
