f4efb0a23410b71250c56b312db78a2198678cdd
Jason van Zyl <jason.vanzyl@fireeye.com>
2019-07-14 19:13:32 -0400

    Support interpolation in configuration files.
    Something like the following will now work:
    
    application {
      x = 123
      y = ${x}
    }

--- ollie/src/test/java/com/walmartlabs/ollie/config/ConfigurationProcessorTest.java
+++ ollie/src/test/java/com/walmartlabs/ollie/config/ConfigurationProcessorTest.java
@@ -50,7 +50,8 @@ public class ConfigurationProcessorTest {
     assertEquals("dev-settle-token", config.getString("approver.settle.token"));    
     assertEquals("dev-jira-username", config.getString("jira.username"));
     assertEquals("dev-jira-password", config.getString("jira.password"));    
-    assertEquals("caring", config.getString("sharing"));    
+    assertEquals("caring", config.getString("sharing"));
+    assertEquals(123, config.getInt("y"));
   }  
 
   @Test
