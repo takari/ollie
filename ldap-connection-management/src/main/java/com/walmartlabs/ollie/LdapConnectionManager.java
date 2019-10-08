069dee63955fab76cf4af2b84029d30c1c981e38
r0s012h <ryan.savage@walmart.com>
2019-10-08 10:35:35 -0500

    optimize search

--- ldap-connection-management/src/main/java/com/walmartlabs/ollie/LdapConnectionManager.java
+++ ldap-connection-management/src/main/java/com/walmartlabs/ollie/LdapConnectionManager.java
@@ -14,41 +14,39 @@ import java.util.List;
 public class LdapConnectionManager {
 
     private List<String> ldapServers;
-    private List<String> failedLdapServers;
+    private int currentServerIndex;
     private final int maxRetries;
     private final static Logger logger = LoggerFactory.getLogger(LdapConnectionManager.class);
 
     public LdapConnectionManager(List<String> ldapServers, int maxRetries) {
-        this.ldapServers = ldapServers;
-        this.failedLdapServers = new ArrayList<>();
+        this.ldapServers = ldapServers == null ? new ArrayList<>() : ldapServers;
+        this.currentServerIndex = 0;
         this.maxRetries = maxRetries;
     }
 
-    public LdapContext getLdapCtxInstance(Hashtable<String, String> props) throws CommunicationException {
+    public LdapContext getLdapCtxInstance(Hashtable<String, String> props) throws NamingException {
         int retries = 0;
         while (retries < maxRetries) {
-            for (String s : ldapServers) {
+            while (currentServerIndex < ldapServers.size()) {
+                String serverName = ldapServers.get(currentServerIndex);
                 try {
-                    LdapContext ctx = (LdapContext) LdapCtxFactory.getLdapCtxInstance(s, props);
+                    LdapContext ctx = (LdapContext) LdapCtxFactory.getLdapCtxInstance(serverName, props);
+                    logger.debug("Ldap connection to {} -> success", serverName);
                     return ctx;
                 } catch (NamingException e) {
                     logger.debug("failed to connect to {}. Trying next ldap server.");
-                    ldapServers.remove(s);
-                    failedLdapServers.add(s);
+                    if (currentServerIndex == ldapServers.size() - 1 && retries == maxRetries) {
+                        logger.warn("Failed to connect to all known servers: {}", e.getExplanation());
+                        throw e;
+                    }
+                    currentServerIndex++;
                 }
             }
             logger.debug("All known servers have at one point failed. Retrying all servers. Attempt {} out of {}", retries + 1, maxRetries);
-            refreshFailedServers();
+            currentServerIndex = 0;
             retries++;
         }
         logger.info("Unable to make a connection to any known Ldap server");
         throw new CommunicationException("Unable to connect to ldap servers");
-    }
-
-    private void refreshFailedServers() {
-        for (int i = failedLdapServers.size() - 1; i >= 0; i--) {
-            ldapServers.add(failedLdapServers.get(i));
-            failedLdapServers.remove(i);
-        }
     }
 }
