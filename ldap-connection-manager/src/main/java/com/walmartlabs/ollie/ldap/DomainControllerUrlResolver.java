e4ea83829b08ed9594b767a8498e11126021b076
ryan savage <ryssavage@byu.edu>
2019-10-24 10:49:37 -0500

    ldap connection management (#29)

---
+++ ldap-connection-manager/src/main/java/com/walmartlabs/ollie/ldap/DomainControllerUrlResolver.java
@@ -0,0 +1,59 @@
+package com.walmartlabs.ollie.ldap;
+
+/*-
+ * *****
+ * Ollie
+ * -----
+ * Copyright (C) 2018 - 2019 Takari
+ * -----
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * =====
+ */
+
+import javax.naming.Context;
+import javax.naming.NamingException;
+import javax.naming.directory.Attribute;
+import javax.naming.directory.Attributes;
+import javax.naming.directory.DirContext;
+import javax.naming.directory.InitialDirContext;
+import java.util.ArrayList;
+import java.util.Hashtable;
+import java.util.List;
+
+public class DomainControllerUrlResolver {
+
+    /**
+     * finds all domain controllers from a given SRV lookup query.
+     *
+     * @param srvQuery
+     * @return
+     * @throws NamingException if domain is not responsive
+     */
+    public List<String> findLDAPServersInWindowsDomain(String srvQuery) throws NamingException {
+        List<String> servers = new ArrayList<String>();
+        Hashtable<String, String> env = new Hashtable<String, String>();
+        env.put(Context.INITIAL_CONTEXT_FACTORY, "com.sun.jndi.dns.DnsContextFactory");
+        env.put("java.naming.provider.url", "dns:");
+        DirContext ctx = new InitialDirContext(env);
+        Attributes attributes = ctx.getAttributes(srvQuery, new String[]{"SRV"}); // that's how Windows domain controllers are registered in DNS
+        Attribute a = attributes.get("SRV");
+        for (int i = 0; i < a.size(); i++) {
+            String srvRecord = a.get(i).toString();
+            // each SRV record is in the format "0 100 389 dc1.company.com."
+            // priority weight port server (space separated)
+            servers.add(srvRecord.split(" ")[3]);
+        }
+        ctx.close();
+        return servers;
+    }
+}
---
+++ ldap-connection-manager/src/test/java/com/walmartlabs/ollie/LdapConnectionManagerTest.java
@@ -0,0 +1,112 @@
+package com.walmartlabs.ollie;
+
+/*-
+ * *****
+ * Ollie
+ * -----
+ * Copyright (C) 2018 - 2019 Takari
+ * -----
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * =====
+ */
+
+import com.walmartlabs.ollie.ldap.LdapConnectionManager;
+import com.walmartlabs.ollie.ldap.LdapCtxFactoryWrapper;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import javax.naming.AuthenticationException;
+import javax.naming.CommunicationException;
+import javax.naming.NamingException;
+import javax.naming.ldap.LdapContext;
+import java.util.Arrays;
+import java.util.Hashtable;
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+import static org.mockito.Matchers.any;
+import static org.mockito.Matchers.eq;
+import static org.mockito.Mockito.*;
+
+public class LdapConnectionManagerTest {
+
+    private LdapConnectionManager connectionManager;
+    @Mock
+    private LdapContext ctx1;
+    @Mock
+    private LdapContext ctx2;
+    @Mock
+    private LdapContext ctx3;
+    @Mock
+    private LdapCtxFactoryWrapper ctxFactory;
+
+    private Hashtable<String, String> goodProps;
+    private Hashtable<String, String> badProps;
+
+    @Before
+    public void setup() throws NamingException {
+        MockitoAnnotations.initMocks(this);
+        goodProps = new Hashtable<>();
+        badProps = new Hashtable<>();
+        goodProps.put("user", "goodUser");
+        goodProps.put("pass", "badPass");
+        badProps.put("user", "badUser");
+        badProps.put("user", "badUser");
+        List<String> servers = Arrays.asList("server1", "server2", "server3");
+        connectionManager = new LdapConnectionManager(servers, 2, ctxFactory);
+
+        when(ctxFactory.getLdapCtxInstance("server1", goodProps)).thenThrow(new CommunicationException());
+        when(ctxFactory.getLdapCtxInstance("server2", goodProps)).thenReturn(ctx2);
+        when(ctxFactory.getLdapCtxInstance("server3", goodProps)).thenReturn(ctx3);
+        when(ctxFactory.getLdapCtxInstance(any(), eq(badProps))).thenThrow(new AuthenticationException());
+
+
+    }
+
+    @Test
+    public void goodCtxTest() throws NamingException {
+        LdapContext context = connectionManager.getLdapCtxInstance(goodProps);
+        assertEquals(ctx2, context);
+    }
+
+    @Test
+    public void badAuthTest() throws NamingException {
+        try {
+            connectionManager.getLdapCtxInstance(badProps);
+        } catch (Exception e) {
+            assertTrue(e instanceof AuthenticationException);
+        }
+        //first server fails. doesn't move on to next server
+        verify(ctxFactory, times(1)).getLdapCtxInstance(any(), any());
+    }
+
+    @Test
+    public void serverReviveTest() throws NamingException {
+        when(ctxFactory.getLdapCtxInstance("server2", goodProps)).thenThrow(new CommunicationException());
+        LdapContext context = connectionManager.getLdapCtxInstance(goodProps);
+        assertEquals(ctx3, context);
+
+        reset(ctxFactory);
+
+        //now that 1 and 2 have previously failed lets make 1 the only good server
+        when(ctxFactory.getLdapCtxInstance("server2", goodProps)).thenThrow(new CommunicationException());
+        when(ctxFactory.getLdapCtxInstance("server3", goodProps)).thenThrow(new CommunicationException());
+        when(ctxFactory.getLdapCtxInstance("server1", goodProps)).thenReturn(ctx1);
+
+        //assert that it retries previously failing server1
+        assertEquals(ctx1, connectionManager.getLdapCtxInstance(goodProps));
+    }
+}
